f2fa0c2454fa415d6749a074b87ff856e8845226
Norris <norrisnakagaki@gmail.com>
2014-09-16 12:07:38 -0400

    Add moves methods for sliding pieces

--- chess.rb
+++ chess.rb
@@ -1,33 +1,74 @@
 class Board
 
+
+  def self.make_board
+    Array.new(8) { Array.new(8) }
+  end
+
+  def initialize
+    @grid = Board.make_board
+  end
+
 end
 
 class Piece
   attr_reader :pos
 
-  def initialize(pos, board)
+  def initialize(pos, board, color)
     @pos, @board = pos, board
   end
 
-  def move
+  def take_move
 
   end
 end
 
 class SlidingPiece < Piece
+  def moves
+    moves = []
 
+    i, j = pos
+    self.directions.each do |dir|
+      8.times do |mag|
+        next if mag.zero?
+        delta_i, delta_j = dir[0] * mag, dir[1] * mag
+        new_pos = [i + delta_i, j + delta_j]
+        break unless new_pos.all? { |coord| coord.between?(0, 7) }
+
+        moves << new_pos
+      end
+    end
+
+    moves
+  end
 end
 
 class Bishop < SlidingPiece
+  DIRECTIONS = [ [1, 1], [-1, -1], [1, -1], [-1, 1] ]
 
+  def directions
+    DIRECTIONS
+  end
 end
 
 class Rook < SlidingPiece
+  DIRECTIONS = [ [1, 0], [-1, 0], [0, -1], [0, 1] ]
 
+  def directions
+    DIRECTIONS
+  end
 end
 
 class Queen < SlidingPiece
+  DIRECTIONS = [
+    [-1, -1], [-1, 0], [-1, 1],
+    [0, -1], [0, 1],
+    [1, -1], [1, 0], [1, 1]
+  ]
 
+  def directions
+    DIRECTIONS
+  end
 end
 
 class SteppingPiece < Piece
